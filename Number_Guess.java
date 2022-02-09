067f6e49cf076e9c1a844bd9542aaafc38cc53e3
Austin Wang <auswang@iu.edu>
2022-02-09 10:59:12 -0500

    moving to github

---
+++ Number_Guess.java
@@ -0,0 +1,27 @@
+/*
+ * The code below is a guessing game of high or low with a random number generator.
+ * this project is from my undergraduate course at IUPUI that was taught in Python where I converted into Java.
+ *
+ * 
+ * Author: Austin Wang
+ * Date Created: February 9, 2022
+ *
+ */
+
+import java.util.Scanner;
+
+public class Number_Guess{
+
+	public static void main(String args[]){
+
+		Scanner User_Input = new Scanner(System.in);
+		System.out.println("This is a number guessing game. \n The program will let you know if your value is high or low.");
+
+		int Random_Value = int (Math.random() * 100 + 1 );
+		int User_Guess = 0;
+		int Number_Attempts = 1;
+
+
+
+	}
+
